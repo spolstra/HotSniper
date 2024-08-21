207ef6ae033e7affc3d19237a484b8db2f70778b
Simon Polstra <s.polstra@uva.nl>
2024-08-21 14:13:17 +0200

    Changed to 16 core platform.

--- simulationcontrol/config.py
+++ simulationcontrol/config.py
@@ -4,7 +4,7 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 SNIPER = os.path.dirname(HERE)
 
 RESULTS_FOLDER = os.path.join(SNIPER, 'results')
-NUMBER_CORES = 4
+NUMBER_CORES = 16
 SNIPER_CONFIG = 'gainestown'
 ENABLE_HEARTBEATS = False
 SCRIPT='magic_timestamp' if ENABLE_HEARTBEATS else ''
